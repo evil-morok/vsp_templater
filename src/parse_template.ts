d5ded71786618376f13f52e78dc2a0ec1d98c871
lobakin <lobakin@uniscan.biz>
2026-03-13 13:18:51 +0700

    switch to handlebars

--- src/parse_template.ts
+++ src/parse_template.ts
@@ -1,41 +1,50 @@
 import * as fs from 'fs';
+import * as Handlebars from 'handlebars';
 import * as vscode from 'vscode';
 
-export function parseTemplate(inputFile: string, outputFile: string, data: Record<string, string>): void {
-    try {
-        let content = fs.readFileSync(inputFile, 'utf-8');
-        
-        for (const [key, value] of Object.entries(data)) {
-            const regex = new RegExp(`\{\{\{${key}\}\}\}`, 'g');
-            content = content.replace(regex, value);
-        }
-        
-        fs.writeFileSync(outputFile, content, 'utf-8');
-    } catch (error) {
-        vscode.window.showErrorMessage(`Error processing the file: ${error}`);
-        console.error('Error processing the file:', error);
-    }
+export async function parseTemplate(inputFile: string, outputFile: string,
+                                    data: Record<string, string>):
+    Promise<void> {
+  try {
+    const inputUri = vscode.Uri.file(inputFile);
+    const fileContent = await vscode.workspace.fs.readFile(inputUri);
+    let content = new TextDecoder().decode(fileContent);
+
+    const template = Handlebars.compile(content);
+    content = template(data);
+
+    const outputUri = vscode.Uri.file(outputFile);
+    await vscode.workspace.fs.writeFile(outputUri,
+                                        new TextEncoder().encode(content));
+
+  } catch (error) {
+    vscode.window.showErrorMessage(`Error processing the file: ${error}`);
+    console.error('Error processing the file:', error);
+  }
 }
 
-export function mergeDictionaries(substitutions: Record<string, Object>, overwrites: Record<string, string>): Record<string, string> {
-    let merged = { ...substitutions["Defaults"] } as Record<string, string>;
-    
-    for (const [key, value] of Object.entries(overwrites)) {
-        if ( key === "$inherits" ) {
-            for (const [iKey, iValue ] of Object.entries(substitutions)) {
-                if (iKey === value) {
-                    merged = {...merged, ...iValue as Record<string, string>};
-                }
-            }
+export function mergeDictionaries(substitutions: Record<string, Object>,
+                                  overwrites: Record<string, string>):
+    Record<string, string> {
+  let merged = {...substitutions["Defaults"]} as Record<string, string>;
+
+  for (const [key, value] of Object.entries(overwrites)) {
+    if (key === "$inherits") {
+      for (const [iKey, iValue] of Object.entries(substitutions)) {
+        if (iKey === value) {
+          merged = {...merged, ...iValue as Record<string, string>};
         }
-        // if (!(key in defaults)) {
-        //     vscode.window.showWarningMessage(`Warning: Key '${key}' does not exist in defaults.`);
-        //     console.warn(`Warning: Key '${key}' does not exist in defaults.`);
-        // }
-        merged[key] = value;
+      }
     }
-    
-    return merged;
+    // if (!(key in defaults)) {
+    //     vscode.window.showWarningMessage(`Warning: Key '${key}' does not
+    //     exist in defaults.`); console.warn(`Warning: Key '${key}' does not
+    //     exist in defaults.`);
+    // }
+    merged[key] = value;
+  }
+
+  return merged;
 }
 
 // Example usage:
